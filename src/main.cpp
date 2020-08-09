aa3cd0aea2a29bea2a99319d93721bc01bd76034
Miklik <dev@miklik.cz>
2020-08-09 21:08:27 +0200

    Control two motors with PID - moveTo 1000 and -1000.

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,8 @@ void isr_process_encoder2(void)
   }
 }
 
+long lasttime = millis();
+
 void setup()
 {
   attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
@@ -44,54 +46,48 @@ void setup()
   TCCR2A = _BV(WGM21) | _BV(WGM20);
   TCCR2B = _BV(CS21);
   
-  //Encoder_1.reset(SLOT1);
-  //Encoder_1.setTarPWM(100);
-  //Encoder_1.move(100,100);
-  //Encoder_1.setMotorPwm(100);
-  //Encoder_2.move(300,200);
-  Serial.print("Slot: ");
-  Serial.print(Encoder_1.getSlotNum());
-  Serial.print(" Int: ");
-  Serial.print(Encoder_1.getIntNum());
-  Serial.print(" portA: ");
-  Serial.print(Encoder_1.getPortA());
-  Serial.print(" portB: ");
-  Serial.println(Encoder_1.getPortB());
-  Serial.print("PulsePos: ");
-  Serial.print(Encoder_1.getPulsePos());
-  Serial.print(" speed: ");
-  Serial.print(Encoder_1.getCurrentSpeed());
-  Serial.print(" CurPWM: ");
-  Serial.print(Encoder_1.getCurPwm());
-  Serial.print(" CurPos: ");
-  Serial.println(Encoder_1.getCurPos());
-  //delay(500);
-  //Encoder_1.setMotionMode(PID_MODE);
-  Encoder_1.setMotorPwm(200);
-  Encoder_1.moveTo(1000,100);
+  Encoder_1.setMotionMode(PID_MODE);
+  Encoder_2.setMotionMode(PID_MODE);
+  Encoder_1.setPulse(7);
+  Encoder_2.setPulse(7);
+  Encoder_1.setRatio(26.9);
+  Encoder_2.setRatio(26.9);
+  Encoder_1.setPosPid(1.8,0,1.2);
+  Encoder_2.setPosPid(1.8,0,1.2);
+  Encoder_1.setSpeedPid(0.18,0,0);
+  Encoder_2.setSpeedPid(0.18,0,0);
+  Encoder_1.setPulsePos(0);
+  Encoder_2.setPulsePos(0);
+
+  Encoder_1.moveTo(1000,150);
+  Encoder_2.moveTo(-1000,150);
 }
 
-void loop()
-{
-  //Encoder_1.setMotorPwm(100);
-  //Encoder_2.setMotorPwm(100);
-  //Encoder_1.updateCurPos();
-  //Encoder_1.updateSpeed();
-  //Encoder_2.updateCurPos();
-  //Serial.print("Spped 1:");
-  //Serial.print(Encoder_1.getCurrentSpeed());
-  //Serial.print(" ,Spped 2:");
-  //Serial.println(Encoder_2.getCurrentSpeed());
-  Serial.print("PulsePos: ");
-  Serial.print(Encoder_1.getPulsePos());
-  Serial.print(" speed: ");
-  Serial.print(Encoder_1.getCurrentSpeed());
-  Serial.print(" CurPWM: ");
-  Serial.print(Encoder_1.getCurPwm());
-  Serial.print(" goTo: ");
-  Serial.print(Encoder_1.distanceToGo());
-  Serial.print(" CurPos: ");
-  Serial.println(Encoder_1.getCurPos());
+void loop() {
+  if (millis() - lasttime > 500) {
+    Serial.print("E1: PulsePos: ");
+    Serial.print(Encoder_1.getPulsePos());
+    Serial.print(" speed: ");
+    Serial.print(Encoder_1.getCurrentSpeed());
+    Serial.print(" CurPWM: ");
+    Serial.print(Encoder_1.getCurPwm());
+    Serial.print(" goTo: ");
+    Serial.print(Encoder_1.distanceToGo());
+    Serial.print(" CurPos: ");
+    Serial.print(Encoder_1.getCurPos());
+    Serial.print(",E2: PulsePos: ");
+    Serial.print(Encoder_2.getPulsePos());
+    Serial.print(" speed: ");
+    Serial.print(Encoder_2.getCurrentSpeed());
+    Serial.print(" CurPWM: ");
+    Serial.print(Encoder_2.getCurPwm());
+    Serial.print(" goTo: ");
+    Serial.print(Encoder_2.distanceToGo());
+    Serial.print(" CurPos: ");
+    Serial.println(Encoder_2.getCurPos());
+    lasttime = millis();
+  }  
   Encoder_1.loop();
+  Encoder_2.loop();
   //delay(100);
 }
