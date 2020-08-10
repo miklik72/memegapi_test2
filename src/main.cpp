550715d1ffc706621899816a240e222b8a218e29
Miklik <dev@miklik.cz>
2020-08-10 21:50:59 +0200

    Added sonic sensor and 7Sdisp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 #include <MeMegaPi.h>
 
-#include <MeMegaPi.h>
-#define DEBUG_INFO
-
 MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
+MeEncoderOnBoard Encoder_3(SLOT3);
+MeUltrasonicSensor sonic(PORT_6);
+Me7SegmentDisplay disp(PORT_8);
 
 void isr_process_encoder1(void)
 {
@@ -31,63 +31,100 @@ void isr_process_encoder2(void)
   }
 }
 
+void isr_process_encoder3(void)
+{
+  if(digitalRead(Encoder_3.getPortB()) == 0)
+  {
+    Encoder_3.pulsePosMinus();
+  }
+  else
+  {
+    Encoder_3.pulsePosPlus();
+  }
+}
+
 long lasttime = millis();
 
 void setup()
 {
   attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
   attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
+  attachInterrupt(Encoder_3.getIntNum(), isr_process_encoder3, RISING);
   Serial.begin(115200);
+  disp.init();
+  disp.set(BRIGHTNESS_2);
   
   //Set PWM 8KHz
-  TCCR1A = _BV(WGM10);
+  TCCR1A = _BV(WGM10);//PIN12
   TCCR1B = _BV(CS11) | _BV(WGM12);
 
-  TCCR2A = _BV(WGM21) | _BV(WGM20);
+  TCCR2A = _BV(WGM21) | _BV(WGM20);//PIN8
   TCCR2B = _BV(CS21);
+
+  TCCR3A = _BV(WGM30);//PIN9
+  TCCR3B = _BV(CS31) | _BV(CS30) | _BV(WGM32);
+   
+  TCCR4A = _BV(WGM40);//PIN5
+  TCCR4B = _BV(CS41) | _BV(CS40) | _BV(WGM42);
   
   Encoder_1.setMotionMode(PID_MODE);
   Encoder_2.setMotionMode(PID_MODE);
+  Encoder_3.setMotionMode(PID_MODE);
   Encoder_1.setPulse(7);
   Encoder_2.setPulse(7);
+  Encoder_3.setPulse(7);
   Encoder_1.setRatio(26.9);
   Encoder_2.setRatio(26.9);
+  Encoder_3.setRatio(26.9);
   Encoder_1.setPosPid(1.8,0,1.2);
   Encoder_2.setPosPid(1.8,0,1.2);
+  Encoder_3.setPosPid(1.8,0,1.2);
   Encoder_1.setSpeedPid(0.18,0,0);
   Encoder_2.setSpeedPid(0.18,0,0);
+  Encoder_3.setSpeedPid(0.18,0,0);
   Encoder_1.setPulsePos(0);
   Encoder_2.setPulsePos(0);
+  Encoder_3.setPulsePos(0);
 
-  Encoder_1.moveTo(1000,150);
-  Encoder_2.moveTo(-1000,150);
+  //Encoder_1.moveTo(1000,150);
+  //Encoder_2.moveTo(-1000,150);
+  //Encoder_3.moveTo(500,100);
 }
 
 void loop() {
   if (millis() - lasttime > 500) {
-    Serial.print("E1: PulsePos: ");
-    Serial.print(Encoder_1.getPulsePos());
+    
+    //uint8_t ListDisp[4];
+    uint16_t distanc = (uint16_t)sonic.distanceCm();
+    disp.display(distanc);      
+
+    Serial.print("E3: PulsePos: ");
+    Serial.print(Encoder_3.getPulsePos());
     Serial.print(" speed: ");
-    Serial.print(Encoder_1.getCurrentSpeed());
+    Serial.print(Encoder_3.getCurrentSpeed());
     Serial.print(" CurPWM: ");
-    Serial.print(Encoder_1.getCurPwm());
+    Serial.print(Encoder_3.getCurPwm());
     Serial.print(" goTo: ");
-    Serial.print(Encoder_1.distanceToGo());
+    Serial.print(Encoder_3.distanceToGo());
     Serial.print(" CurPos: ");
-    Serial.print(Encoder_1.getCurPos());
-    Serial.print(",E2: PulsePos: ");
-    Serial.print(Encoder_2.getPulsePos());
-    Serial.print(" speed: ");
-    Serial.print(Encoder_2.getCurrentSpeed());
-    Serial.print(" CurPWM: ");
-    Serial.print(Encoder_2.getCurPwm());
-    Serial.print(" goTo: ");
-    Serial.print(Encoder_2.distanceToGo());
-    Serial.print(" CurPos: ");
-    Serial.println(Encoder_2.getCurPos());
+    Serial.print(Encoder_3.getCurPos());
+    // Serial.print(",E2: PulsePos: ");
+    // Serial.print(Encoder_2.getPulsePos());
+    // Serial.print(" speed: ");
+    // Serial.print(Encoder_2.getCurrentSpeed());
+    // Serial.print(" CurPWM: ");
+    // Serial.print(Encoder_2.getCurPwm());
+    // Serial.print(" goTo: ");
+    // Serial.print(Encoder_2.distanceToGo());
+    // Serial.print(" CurPos: ");
+    // Serial.println(Encoder_2.getCurPos());
+    Serial.print(" distance: ");
+    Serial.print(distanc);
+    Serial.println("");
     lasttime = millis();
   }  
   Encoder_1.loop();
   Encoder_2.loop();
+  Encoder_3.loop();
   //delay(100);
 }
